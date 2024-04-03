3ae6edad8950b5d214e393ca64e065ed5fb7dfad
antden99 <antoniodenigris@icloud.com>
2024-04-03 13:07:58 +0200

    add Milestone 2 add the sorted list to view the movies once I've done a search

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ export default {
   data() {
     return {
       state: state,    //alla proprietà state assegno state, che sarebbe la costante importata
+
     }
   },
   //con mounted al caricamento dei dati, prima di essere caricati in pagina, vado a recuperare l'array di oggetti card, tramite il localhost che ho generato nel passaggio precedente con npm run serve(vedi packge.json)
@@ -25,13 +26,27 @@ export default {
   <div>
 
     <!--aggiungo l'input ed il button per visualizzarli in pagina per la milestone 1-->
-    <input type="text" placeholder="cerca il tuo film" v-model="state.searchMovie" >
+    <input type="text" placeholder="cerca il tuo film" v-model="state.searchMovie">
 
     <!--aggiungo l'evento click, che si attiva quando clicco sul bottone e attiva la funzione dichiarata nello state chiamata -->
     <button @click="state.research">Invia</button>
 
-    
-
+    <ol>
+      <li v-for="film in state.filmsList">
+        <div>
+          {{ "titolo: " + film.title }}
+        </div>
+        <div>
+          {{ "titolo originale:" + film.original_title }}
+        </div>
+        <div>
+          {{ "lingua originale:" + film.original_language }}
+        </div>
+        <div>
+          {{ "voto:" + film.vote_average }}
+        </div>
+      </li>
+    </ol>
 
   </div>
 </template>
--- src/state.js
+++ src/state.js
@@ -7,32 +7,34 @@ import { reactive } from 'vue';
 
 //rendo possibile con export la constante state a cui assegno l'oggetto reactive
 export const state = reactive({
-
     //dichiaro una proprietà con all'interno il mio indirizzo principale con la mia chiave
-    address_my_key:"https://api.themoviedb.org/3/search/movie?api_key=f816e22efeac8c7bafa0b02db3a618e3",
-
+    address_my_key: "https://api.themoviedb.org/3/search/movie?api_key=f816e22efeac8c7bafa0b02db3a618e3",
     //dichiaro una proprietà inizialmente vuota che colleghero con la modale nel App.vue principale
     searchMovie: "",
-   
+    //dichiato una proprietà che contiene un array inizialmente vuoto e che andrò a riempire successivamente con la chiamata ajax fatta in research() 
+    filmsList: [],
+
     //dichiaro il metodo callApifunction che mi permette, tramite la mia chiamata ajax di accedere alla lista dei film
     callApifunction(url) {
-
-        //alla chiamata di questa funzione invoco ajax che mi permette, tramite l'http ricavato all'inizio tramite il db.jeson, di recuperare un array di oggetti che sti trova in response.data, e che successivamente assegno all'array cardsList_2
-        axios
+        // Restituisco una promessa tramite il return per consentire la gestione asincrona dei dati
+        return axios
             .get(url)
             .then(response => {
-                console.log(response); //faccio il console di responde.data, dove sono contenuti i miei film
+                this.filmsList = response.data.results;
+                return response.data.results; // Restituisci i dati aggiornati per la gestione successiva
             })
     },
 
     //aggiungo la funzione research, che si attiva quando effettuo il click sul bottone in App.vue e ricerca con l'aggiunta di una query, se i caratteri inseriti sono presenti nel db tramite l'api
-    research(){
-        console.log(this.searchMovie)
+    research() {
+        console.log("Stai cercando : " + this.searchMovie);
 
-
+        // Utilizzo la promessa restituita dalla funzione callApifunction
         this.callApifunction(`${this.address_my_key}&query=${this.searchMovie}`)
-    }
-
-    
+            .then(() => {
+                // dopo che ho ricevuto i dati aggiornati dalla promessa, stampo in console il risultato
+                console.log(this.filmsList);
+            });
+    },
 
 })
